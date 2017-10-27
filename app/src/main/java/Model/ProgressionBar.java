d0858ed22d8c0c179dd2fba48130e9f22713672b
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-10-26 21:01:33 -0700

    created custom action bar, need to add buttons and hamburger menu

---
+++ app/src/main/java/Model/ProgressionBar.java
@@ -0,0 +1,9 @@
+package Model;
+
+/**
+ * Created by brandonholderman on 10/26/17.
+ */
+
+public class ProgressionBar {
+
+}
--- app/src/main/java/com/example/louis/myapplication/MainActivity.java
+++ app/src/main/java/com/example/louis/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.louis.myapplication;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
     }
 }
