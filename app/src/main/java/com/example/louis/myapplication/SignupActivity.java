69ff112dc6b64b6347bfc096b65360ab06ddcb9a
Steven Johnson <stevenjohnson86@gmail.com>
2017-10-28 11:18:04 -0700

    added SignupActivity

--- app/src/main/java/com/example/louis/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/louis/myapplication/SignupActivity.java
@@ -1,14 +1,20 @@
 package com.example.louis.myapplication;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignupActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
@@ -31,18 +37,22 @@ public class SignupActivity extends AppCompatActivity {
         mSignup = (Button) findViewById(R.id.signup_submit_btn);
         mGotoLogin = (TextView) findViewById(R.id.signup_goto_login_textView);
 
-
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user != null){
-                    //user is logged in, goto HomeActy
-//                    finish();
+                    // entry point for app is HomeActy, so even if a user somehow gets to the login, it will kick them back to homeActy (so no need to worry about erasing a users displayName)
+                    UserProfileChangeRequest req = new UserProfileChangeRequest.Builder().setDisplayName(mUsername.getText().toString()).build();
+                    user.updateProfile(req);
+
+                    //user is logged in, go back to Login (and from there go to HomeActy)
+                    finish();
                 }
             }
         };
+        attachListeners();
     }
 
     @Override
@@ -55,5 +65,70 @@ public class SignupActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    public void attachListeners(){
+        mSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismissKeyboard(view);
+
+                String email = mEmail.getText().toString();
+                String un = mUsername.getText().toString();
+                String pw = mPassword.getText().toString();
+                if (email.length() < 7 || un.length() < 7 || pw.length() < 7){
+                    Toast.makeText(SignupActivity.this, "Signup Failed. Ensure you are using a valid email and your username and password are both 8 characters or longer.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                try {
+                    mAuth.createUserWithEmailAndPassword(email, pw);
+                } catch (Exception e){
+                    Toast.makeText(SignupActivity.this, "Something failed in the Auth process.\nPlease try again or contact support.", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        mGotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go back to Login
+                finish();
+            }
+        });
+
+        mEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(!b) {
+                    dismissKeyboard(view);
+                }
+
+            }
+        });
+
+        mUsername.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(!b) {
+                    dismissKeyboard(view);
+                }
+            }
+        });
+
+        mPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(!b) {
+                    dismissKeyboard(view);
+                }
+            }
+        });
+    }
+
+    private void dismissKeyboard(View view){
+            InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
     }
 }
