82effb70c946b371d72e1b0960b7bf7b22963143
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-10-28 15:19:22 -0700

    resolved issue of menu not working

--- app/src/main/java/com/example/louis/myapplication/MainActivity.java
+++ app/src/main/java/com/example/louis/myapplication/MainActivity.java
@@ -8,8 +8,11 @@ public class MainActivity extends MenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+
+
+        super.onCreate(savedInstanceState);
     }
 }
--- app/src/main/java/com/example/louis/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/louis/myapplication/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.louis.myapplication;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.view.View;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -36,8 +38,28 @@ public class MenuActivity extends AppCompatActivity {
 //        return true;
 //    }
 
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        return mToggle.onOptionsItemSelected(item);
+//    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return mToggle.onOptionsItemSelected(item);
+        mToggle.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.tasks:
+                 Intent detailIntent = new Intent(this, DetailActivity.class);
+                 startActivity(detailIntent);
+                return true;
+            case R.id.home:
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                startActivity(homeIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
+
+    dr
 }
