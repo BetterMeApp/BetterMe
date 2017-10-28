79864185aa2dc3b2357b90c6877a257838472c59
Paul <paulhise1@gmail.com>
2017-10-28 14:47:39 -0700

    set up task object class and its constructor

--- app/src/main/java/Model/Task.java
+++ app/src/main/java/Model/Task.java
@@ -1,8 +1,28 @@
 package Model;
 
+import java.sql.Date;
+import java.sql.Time;
+
 /**
  * Created by brandonholderman on 10/26/17.
  */
 
 public class Task {
+
+    String mTaskTitle;
+    String mDescription;
+    Time mStartTime;
+    Date mStartDate;
+    Integer mGoalNumber;
+    Integer mCompletedNumber;
+
+    public Task(String title, String description, Time time, Date date, Integer goal, Integer completed){
+        this.mTaskTitle = title;
+        this.mDescription = description;
+        this.mStartTime = time;
+        this.mStartDate = date;
+        this.mGoalNumber = goal;
+        this.mCompletedNumber = completed;
+    }
+
 }
