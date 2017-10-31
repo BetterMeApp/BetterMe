743d6a284ca0af6b03ea229bd9aca8687b4824ac
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-10-30 19:52:46 -0700

    nav drawer working

--- app/src/main/java/Model/ProgressionBar.java
+++ app/src/main/java/Model/ProgressionBar.java
@@ -18,41 +18,41 @@ import butterknife.OnClick;
 
 public class ProgressionBar extends AppCompatActivity {
 
-    @BindView(R.id.progress_amount)
-    TextView mProgressAmount;
-    @BindView(R.id.progress_bar)
-    ProgressBar mProgressBar;
-    @BindView(R.id.start_button)
-    Button mStartButton;
-
-    private int progressStatus = 0;
-    private Handler mHandler = new Handler();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-
-
-    }
-
-    @OnClick(R.id.start_button)
-    public void startButtonPressed() {
-        progressStatus = 0;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(progressStatus < 100) {
-                    progressStatus += 1;
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mProgressBar.setProgress(progressStatus);
-                            mProgressAmount.setText("Progress" + progressStatus + "/" + mProgressBar.getMax());
-                        }
-                    });
-                }
-            }
-        }).start();
-    }
+//    @BindView(R.id.progress_amount)
+//    TextView mProgressAmount;
+//    @BindView(R.id.progress_bar)
+//    ProgressBar mProgressBar;
+//    @BindView(R.id.start_button)
+//    Button mStartButton;
+//
+//    private int progressStatus = 0;
+//    private Handler mHandler = new Handler();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_detail);
+//
+//
+//    }
+//
+//    @OnClick(R.id.start_button)
+//    public void startButtonPressed() {
+//        progressStatus = 0;
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while(progressStatus < 100) {
+//                    progressStatus += 1;
+//                    mHandler.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            mProgressBar.setProgress(progressStatus);
+//                            mProgressAmount.setText("Progress" + progressStatus + "/" + mProgressBar.getMax());
+//                        }
+//                    });
+//                }
+//            }
+//        }).start();
+//    }
 }
--- app/src/main/java/com/example/louis/myapplication/MenuDrawer.java
+++ app/src/main/java/com/example/louis/myapplication/MenuDrawer.java
@@ -1,27 +1,27 @@
 package com.example.louis.myapplication;
 
+import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MenuDrawer extends AppCompatActivity {
+public class MenuDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ActionBarDrawerToggle mToggle;
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
 
     @BindView(R.id.drawer)
     DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.nav_view)
+    NavigationView mNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,59 +29,46 @@ public class MenuDrawer extends AppCompatActivity {
         setContentView(R.layout.navigation_drawer);
         ButterKnife.bind(this);
 
-        mTitle = mDrawerTitle = getTitle();
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close) {
-//        mDrawerLayout.addDrawerListener(mToggle);
-//        mToggle.syncState();
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu();
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu();
-            }
-        };
-
-        mDrawerLayout.setDrawerListener(mToggle);
+        mNavView.setNavigationItemSelectedListener(this);
     }
 
-//    public boolean onPrepareOptionsMenu(Menu menu) {
-//        boolean drawerOpen = mDrawerLayout.isDrawerOpen(m)
-//    }
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.nav_menu, menu);
-//
-//        return true;
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mToggle.onOptionsItemSelected(item);
+    }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        return mToggle.onOptionsItemSelected(item);
-//    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        if (id == R.id.profile) {
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        mToggle.onOptionsItemSelected(item);
-//        switch (item.getItemId()) {
-//            case R.id.tasks:
-//                 Intent detailIntent = new Intent(this, DetailActivity.class);
-//                 startActivity(detailIntent);
-//                return true;
-//            case R.id.home:
-//                Intent homeIntent = new Intent(this, MainActivity.class);
-//                startActivity(homeIntent);
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
+        } else if (id == R.id.home) {
+            Intent homeIntent = new Intent(this, MainActivity.class);
+            startActivity(homeIntent);
+            return true;
+        } else if (id == R.id.tasks) {
+            Intent detailIntent = new Intent(this, DetailActivity.class);
+            startActivity(detailIntent);
+            return true;
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
